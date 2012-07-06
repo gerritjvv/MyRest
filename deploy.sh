2760b48dd8f8f6087783d24ee2c298d10a5fd555
gerritjvv <gerritjvv@gmail.com>
2012-07-06 10:51:22 +0200

    performance release 0.2.1

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-mvn install:install-file  -Dfile=target/myrest-0.2.0.jar \
+mvn install:install-file  -Dfile=target/myrest-0.2.1.jar \
                           -DgroupId=org.myrest \
                           -DartifactId=myrest \
-                          -Dversion=0.2.0 \
+                          -Dversion=0.2.1 \
                           -Dpackaging=jar \
                           -DlocalRepositoryPath=releases
